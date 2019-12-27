1a9e90d3e597423338f6e4bd28b689dd91f5e70f
Shideshan <307042873@qq.com>
2019-12-27 17:25:27 +0800

    init

---
+++ components/public/header/goe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location" />杭州市
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity"
+    >
+      切换城市
+    </nuxt-link>[余杭 西湖 滨江]
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+</style>
